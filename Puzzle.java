489457974c3817cba4a4fedf2d1e654cfac9c586
Baker <BakeLuA@morton709.org>
2024-10-30 09:37:08 -0500

    task 4uh is done
    i have eliminated all threats of palindroms vowels and consecutive letters and finished all the easter eggs to complete the super easter egg.
    
    ..............

--- Puzzle.java
+++ Puzzle.java
@@ -18,56 +18,56 @@ public class Puzzle{
 
 
 
-    // String palen = text;
-    //     int longest = 0;
-    //     int bestindex =0;
-    //     boolean flag = true;
-    //     for(int i=0; i<palen.length(); i++){
-    //         int skib = 1;
-    //         for(int j=1; flag; j++){
-    //             if(i-j<0 || i+j>=palen.length()){
-    //                 flag = false;
-    //             }else{
-    //                 if(palen.substring(i-j,i-j+1).equals(palen.substring(i+j, i+j+1))){
-    //                     skib++;
-    //                     skib++;
-    //                 }else{
-    //                     flag = false;
-    //                 }
-    //             }
-    //         }   
-    //         flag = true;
-    //         int sigma = 0;
-    //         if(i+1<palen.length() && palen.substring(i,i+1).equals(palen.substring(i+1,i+2))){
-    //             sigma = 2;
-    //             for(int j=1; flag; j++){
-    //                 if(i-j<0 || i+j+1>=palen.length()){
-    //                     flag = false;
-    //                 }else{
-    //                     if(palen.substring(i-j,i-j+1).equals(palen.substring(i+j+1, i+j+2))){
-    //                     sigma++;
-    //                     }else{
-    //                     flag = false;
-    //                     }
-    //                 }
-    //             } 
-    //             flag = true;
-    //         }
-    //         if(skib>longest){
-    //             longest = skib;
-    //             bestindex = i-skib/2;
-    //         }
-    //         if(longest<sigma){
-    //             longest = sigma;
-    //             bestindex = i-longest/2+1;
-    //         }
-    //         // System.out.println("Current Index: " + i);
-    //         // System.out.println("Current Best Length: " + longest);
-    //         // System.out.println("");
-    //     }
-        // System.out.println(longest);
-        // System.out.println(bestindex);
-        // System.out.println(palen.substring(bestindex, bestindex+105));
+    String palen = text;
+        int longest = 0;
+        int bestindex =0;
+        boolean flag = true;
+        for(int i=0; i<87300; i++){
+            int skib = 1;
+            for(int j=1; flag; j++){
+                if(i-j<0 || i+j>=palen.length()){
+                    flag = false;
+                }else{
+                    if(palen.substring(i-j,i-j+1).equals(palen.substring(i+j, i+j+1))){
+                        skib++;
+                        skib++;
+                    }else{
+                        flag = false;
+                    }
+                }
+            }   
+            flag = true;
+            int sigma = 0;
+            if(i+1<palen.length() && palen.substring(i,i+1).equals(palen.substring(i+1,i+2))){
+                sigma = 2;
+                for(int j=1; flag; j++){
+                    if(i-j<0 || i+j+1>=palen.length()){
+                        flag = false;
+                    }else{
+                        if(palen.substring(i-j,i-j+1).equals(palen.substring(i+j+1, i+j+2))){
+                        sigma++;
+                        }else{
+                        flag = false;
+                        }
+                    }
+                } 
+                flag = true;
+            }
+            if(skib>longest){
+                longest = skib;
+                bestindex = i-skib/2;
+            }
+            if(longest<sigma){
+                longest = sigma;
+                bestindex = i-longest/2+1;
+            }
+            // System.out.println("Current Index: " + i);
+            // System.out.println("Current Best Length: " + longest);
+            // System.out.println("");
+        }
+        System.out.println(longest);
+        System.out.println(bestindex);
+        System.out.println(palen.substring(bestindex, bestindex+105));
 
         
         // int count = 0;
@@ -90,22 +90,25 @@ public class Puzzle{
         // }
         // System.out.println(mostVowels);
 
-        String palen = "imnopqponmi";
-        String rever = "";
-        String first = "";
-        String gyatt = "";
-        for(int i=0; i<palen.length()-11; i=i+1){
-            first = text.substring(i,i+11);
-            for(int j=first.length(); j>0; j--){
-                rever = rever + text.substring(j-1,j);
-            }
-            if(first.equals(rever)){
-                gyatt = first;
-                break;
-            }
-            System.out.println(i);
-        }
-        System.out.println(gyatt);
+        // String palen = text;
+        // String rever = "";
+        // String first = "";
+        // String gyatt = "";
+        // for(int i=0; i<palen.length()-11; i++){
+            
+        //     first = palen.substring(i,i+10);
+        //     for(int j=0; j<first.length();j++){
+        //         rever = text.substring(j,j+1) + rever;
+        //     }
+        //     if(first.equals(rever)){
+        //         gyatt = first;
+                
+        //     }else{
+        //         rever = "";
+        //     }
+        //     System.out.println(i); 
+        // }
+        // System.out.println(gyatt);
 }
 }
 //armadillo your next task and keyword are located in the one hundred 
@@ -115,3 +118,5 @@ public class Puzzle{
 //after the string of length thirty with the most vowels
 
 //ostrich your final keyword is located in the six characters after a palindrome of length eleven
+
+// badger
